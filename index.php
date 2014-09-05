68d0d68c21b0d78577550d7ee48eb344a60cd832
merkrodi <gavr.93@mail.ru>
2014-09-06 00:32:39 +0400

    start: index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<html>
+
+    <head>
+        <title>Главная страница</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    </head>
+
+
+    <body>
+        <h2>Привет!</h2>
+
+        <br>
+
+        echo "<a href='http://amidaniram.ru/merkrodi/regv1/'>Главная страница</a>";
+
+    </body>
+
+
+</html>
